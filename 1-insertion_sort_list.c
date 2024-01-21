12523ef57325fa2480bf6536fcd3d7d2a0f261b6
Moe-95 <mhammad_fahs@hotmail.com>
2024-01-21 21:54:35 +0200

    2-6

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,43 +1,53 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- * in ascending order using Insertion sort algorithm
+ * insertion_sort_list - Sorts a doubly linked list
+ * of integers in ascending order using
+ * the Insertion sort algorithm.
+ * @list: The doubly linked list to sort
  *
- * @list: Pointer to the head of the doubly linked list
+ * Return: Nothing!
  */
 void insertion_sort_list(listint_t **list)
 {
+bool flag = false;
+listint_t *tmp = NULL, *aux = NULL;
 
-
-listint_t *current, *temp;
-
-current = (*list)->next;
-if (list == NULL || *list == NULL || (*list)->next == NULL)
+if (!list || !(*list) || !(*list)->next)
 return;
-while (current != NULL)
+
+tmp = *list;
+while (tmp->next)
 {
-temp = current;
-
-while (temp->prev != NULL && temp->n < temp->prev->n)
+if (tmp->n > tmp->next->n)
 {
-temp->prev->next = temp->next;
+tmp->next->prev = tmp->prev;
+if (tmp->next->prev)
+tmp->prev->next = tmp->next;
+else
+*list = tmp->next;
 
-if (temp->next != NULL)
-temp->next->prev = temp->prev;
+tmp->prev = tmp->next;
+tmp->next = tmp->next->next;
+tmp->prev->next = tmp;
+if (tmp->next)
+tmp->next->prev = tmp;
 
-temp->next = temp->prev;
-temp->prev = temp->prev->prev;
-
-if (temp->prev != NULL)
-temp->prev->next = temp;
-
-if (temp->prev == NULL)
-*list = temp;
-
+tmp = tmp->prev;
 print_list(*list);
-}
 
-current = current->next;
+if (tmp->prev && tmp->prev->n > tmp->n)
+{
+if (!flag)
+aux = tmp->next;
+flag = true;
+tmp = tmp->prev;
+continue;
+}
+}
+if (!flag)
+tmp = tmp->next;
+else
+tmp = aux, flag = false;
 }
 }
